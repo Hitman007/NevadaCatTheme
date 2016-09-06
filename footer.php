f77c3f684e25a655e9d0fa2b35243befc6a950d2
Hitman007 <support@customrayguns.com>
2016-09-06 10:59:05 -0700

    asias here

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer role="contentinfo">
 <div id="inner-footer" class="wrap clearfix">
-	<span style="float:left;">Copyright <a href = "http://asiascatcreations.com/wp-admin/index.php">&copy;</a> Asia's Cat Creations. All rights reserved. <a href = "http://asiascatcreations.com/contact/">Contact us.</a> Check us out on <a href = "https://www.facebook.com/pages/Asias-Cat-Creations/168526476626573">Facebook</a>. This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "<?php $site_url = get_bloginfo('url'); echo ($site_url . "/wp-content/uploads/2012/12/ray_gun.gif"); ?>" /></a>.</span>
+	<span style="float:left;">Copyright <a href = "http://nevadacathouse.biz/wp-admin/index.php">&copy;</a> Nevada Cat House. All rights reserved. <a href = "http://nevadacathouse.biz/contact/">Contact us.</a> Check us out on <a href = "">Facebook</a>. This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "<?php $site_url = get_bloginfo('url'); echo ($site_url . "/wp-content/uploads/2012/12/ray_gun.gif"); ?>" /></a>.</span>
 </div> <!-- end #inner-footer -->
 </footer> <!-- end footer -->
 
