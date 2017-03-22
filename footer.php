2b8d9c84029eb57965f8844b381ef01d1a804f74
Hitman007 <support@customrayguns.com>
2017-03-22 09:14:06 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer role="contentinfo">
 <div id="inner-footer" class="wrap clearfix">
-	<span style="float:left;">Copyright <a href = "http://nevadacathouse.biz/wp-admin/index.php">&copy;</a> Nevada Cat House. All rights reserved. <a href = "http://nevadacathouse.biz/contact/">Contact us.</a> Check us out on <a href = "">Facebook</a>. This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "<?php $site_url = get_bloginfo('url'); echo ($site_url . "/wp-content/uploads/2012/12/ray_gun.gif"); ?>" /></a>.</span>
+	<span style="float:left;">Copyright <a href = "http://nevadacathouse.biz/wp-admin/index.php">&copy;</a> Nevada Cat House. All rights reserved. <a href = "http://nevadacathouse.biz/contact/">Contact us.</a> This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "<?php $site_url = get_bloginfo('url'); echo ($site_url . "/wp-content/themes/NevadaCatTheme/library/images/ray_gun.gif"); ?>" /></a>.</span>
 </div> <!-- end #inner-footer -->
 </footer> <!-- end footer -->
 
