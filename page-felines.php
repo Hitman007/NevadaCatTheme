7228b5c7f5c2fb6dfa252dc7a0b2c94ac6e109ad
Jim <jiminac@aol.com>
2017-08-19 00:38:55 -0700

    asdf

--- page-felines.php
+++ page-felines.php
@@ -8,15 +8,15 @@
 <header class="article-header">
 <h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 </header> <!-- end article header -->
-<section class="entry-content clearfix" itemprop="articleBody">
+<section class="entry-content clearfix" itemprop="articleBody">HELLO THERE!!
 <div class="page_img_right"><img src = "<?php global $post; $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); echo $url;?>" /></div>
 <?php the_content(); ?>
 </section> <!-- end article section -->
 <footer class="article-footer">
 </footer> <!-- end article footer -->
-<?php endwhile; ?>			
+<?php endwhile; ?>
 <?php else : ?>
-					
+
 							<article id="post-not-found" class="hentry clearfix">
 					    		<header class="article-header">
 					    			<h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
@@ -28,19 +28,19 @@
 					    		    <p><?php _e("This is the error message in the single.php template.", "bonestheme"); ?></p>
 					    		</footer>
 							</article>
-					
+
 						<?php endif; ?>
-			
+
 </div>
 <div class = "twelvecol first">
-<?php comments_template(); ?> 
+<?php comments_template(); ?>
 </div>
 					</div> <!-- end #main -->
 					<?php get_sidebar(); ?>
 
 
 				</div> <!-- end #inner-content -->
- 
+
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>
